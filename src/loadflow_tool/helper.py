a9ea2c7c6b5d02b1f3c11b525e3bed5256d4f960
Vilgo <kristjan.vilgo@elering.ee>
2023-04-14 16:22:11 +0300

    Started migrating code form internal repo to loadflow tool folder Separated examples from code Updated merge task example

---
+++ src/loadflow_tool/helper.py
@@ -0,0 +1,20 @@
+from zipfile import ZipFile
+from uuid import uuid4
+from io import BytesIO
+import logging
+
+logger = logging.getLogger()
+
+# TODO - Addcomments and docstring
+def package_for_pypowsybl(opdm_components):
+
+    with ZipFile(f"{uuid4()}.zip", "w") as global_zip:
+        logging.info(f"Adding files to {global_zip.filename}")
+
+        for instance in opdm_components:
+            with ZipFile(BytesIO(instance['opdm:Profile']['DATA'])) as instance_zip:
+                for file_name in instance_zip.namelist():
+                    logging.info(f"Adding file {file_name}")
+                    global_zip.writestr(file_name, instance_zip.open(file_name).read())
+
+    return global_zip.filename
---
+++ src/loadflow_tool/validator.py
@@ -0,0 +1,43 @@
+import pypowsybl
+from helper import package_for_pypowsybl
+import logging
+
+logger = logging.getLogger()
+
+
+# https://pypowsybl.readthedocs.io/en/stable/reference/loadflow/parameters.html#pypowsybl.loadflow.Parameters
+igm_validation_parameters = pypowsybl.loadflow.Parameters(voltage_init_mode=pypowsybl._pypowsybl.VoltageInitMode.PREVIOUS_VALUES,
+                                                          transformer_voltage_control_on=True,
+                                                          no_generator_reactive_limits=False,
+                                                          phase_shifter_regulation_on=True,
+                                                          #twt_split_shunt_admittance=None,
+                                                          simul_shunt=True,
+                                                          read_slack_bus=True,
+                                                          write_slack_bus=False,
+                                                          distributed_slack=False,
+                                                          balance_type=pypowsybl._pypowsybl.BalanceType.PROPORTIONAL_TO_GENERATION_P_MAX,
+                                                          dc_use_transformer_ratio=None,
+                                                          countries_to_balance=None,
+                                                          connected_component_mode=pypowsybl._pypowsybl.ConnectedComponentMode.MAIN, # ALL / MAIN
+                                                          #provider_parameters=None
+                                                          )
+
+
+def validate_model(model, latest_boundary):
+
+    instance_files = model['opdm:OPDMObject']['opde:Component'] + latest_boundary['opdm:OPDMObject']['opde:Component']
+
+    global_zip_filename = package_for_pypowsybl(instance_files)
+
+    # TODO - get powsybl to use BytesIO object instead of filesystem paths for import
+    network = pypowsybl.network.load(global_zip_filename)
+    print(network)
+
+    # TODO - use PF Parameters as defined in EMF requirements
+    result = pypowsybl.loadflow.run_ac(network)
+    print(result)
+
+    # TODO - record AC NP and DC Flows to metadata storage (and more), this is useful for replacement logic and scaling
+    # note - multiple islands wo load or generation can be an issue
+
+    return {"network": network, "powerflow": result}
