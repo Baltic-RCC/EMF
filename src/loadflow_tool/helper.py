8d8649ef2545f6dc7b928f1bd39286c71e726835
martynas.karobcikas <mkarobcikas@gmail.com>
2023-05-25 12:48:10 +0300

    ammendment: added settings, pp class attribute parser, opdm functions and decorators

--- src/loadflow_tool/helper.py
+++ src/loadflow_tool/helper.py
@@ -3,11 +3,15 @@ from uuid import uuid4
 from io import BytesIO
 import logging
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
-# TODO - Addcomments and docstring
+# TODO - Add comments and docstring
 def package_for_pypowsybl(opdm_components):
-
+    """
+    Method to transform OPDM components into sufficient format zip package
+    :param opdm_components:
+    :return: zip package file name
+    """
     with ZipFile(f"{uuid4()}.zip", "w") as global_zip:
         logging.info(f"Adding files to {global_zip.filename}")
 
@@ -18,3 +22,17 @@ def package_for_pypowsybl(opdm_components):
                     global_zip.writestr(file_name, instance_zip.open(file_name).read())
 
     return global_zip.filename
+
+
+def pp_object_attr_parser(pp_object):
+    """
+    Method to get class variables in dictionary from powsybl object
+    Example: LimitViolation(subject_id='e49a61d1-632a-11ec-8166-00505691de36', subject_name='', limit_type=HIGH_VOLTAGE, limit=450.0, limit_name='', acceptable_duration=2147483647, limit_reduction=1.0, value=555.6890952917897, side=ONE)
+    pypowsybl._pypowsybl.LimitViolation -> dict
+    :param pp_object: powsybl object
+    :return: dict
+    """
+    attribs = [attr for attr in dir(pp_object) if not inspect.ismethod(attr) and not attr.startswith("__")]
+    result_dict = {attr_key: pp_object.__getattribute__(attr_key) for attr_key in attribs}
+
+    return result_dict
