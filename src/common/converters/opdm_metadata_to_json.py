4fe438769dd6e2206b8bc04e5aa760e28f6cb669
Haigutus <vilgo44@hotmail.com>
2023-07-05 02:08:25 +0300

    Create opdm_metadata_to_json.py
    https://github.com/Baltic-RCC/EMF/issues/13

---
+++ src/common/converters/opdm_metadata_to_json.py
@@ -0,0 +1,31 @@
+from xmltodict import parse
+from json import dumps
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def convert(input_document):
+    """
+    Convert XML to JSON
+
+    :param input_document: The incoming XML document
+    :type input_document: str or bytes
+
+    :return: JSON document and Content Type
+    :rtype: bytes
+    """
+    logger.info("OPDM Metadata XML to JSON")
+    return dumps(parse(input_document)["sm:Publish"]['sm:part'][0]['opdm:OPDMObject'], indent=4).encode(), "application/json"
+
+if __name__ == "__main__":
+
+    logging.basicConfig(stream=sys.stdout,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    # TODO - add correct example
+    input_xml = "saxon/model_meta.xml"
+
+    print(convert(open(input_xml, "rb").read())[0].decode())
