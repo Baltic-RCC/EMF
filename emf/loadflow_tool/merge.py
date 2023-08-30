2f6e03b288f5fe07169086454e52a5b87a4a15d8
Vilgo <kristjan.vilgo@elering.ee>
2023-08-30 12:52:46 +0300

    Refactored merge.py moved some functions to helper.py #1

--- emf/loadflow_tool/merge.py
+++ emf/loadflow_tool/merge.py
@@ -1,5 +1,5 @@
 import pypowsybl
-from helper import attr_to_dict, load_model
+from helper import load_model, load_opdm_data, filename_from_metadata
 from validator import validate_model
 import logging
 import uuid
@@ -13,21 +13,9 @@ import os
 import triplets
 import pandas
 import datetime
-import zipfile
-from lxml import etree
 from uuid import uuid4
 
-from io import BytesIO
 # Update SSH
-def opdmprofile_to_bytes(opdm_profile):
-    data = BytesIO(opdm_profile['opdm:Profile']['DATA'])
-    data.name = opdm_profile['opdm:Profile']['pmd:fileName']
-    return data
-
-def load_opdm_data(opdm_objects, profile=None):
-    if profile:
-        return pandas.read_RDF([opdmprofile_to_bytes(instance) for model in opdm_objects for instance in model['opdm:OPDMObject']['opde:Component'] if instance['opdm:Profile']['pmd:cgmesProfile'] == profile])
-    return pandas.read_RDF([opdmprofile_to_bytes(instance) for model in opdm_objects for instance in model['opdm:OPDMObject']['opde:Component']])
 
 logger = logging.getLogger(__name__)
 
@@ -37,167 +25,6 @@ logging.basicConfig(
     level=logging.INFO,
     handlers=[logging.StreamHandler(sys.stdout)]
 )
-
-meta_separator = "_"
-
-
-def metadata_from_filename(file_name):
-
-    file_metadata = {} # Meta container
-
-    file_name, file_metadata["file_type"] = file_name.split(".")
-    meta_list = file_name.split(meta_separator)
-
-    if len(meta_list) == 4:   #try: #if "_EQ_" in file_name or "_BD_" in file_name:
-
-        file_metadata['pmd:validFrom'], model_authority, file_metadata['pmd:cgmesProfile'], file_metadata['pmd:versionNumber'] = meta_list
-        file_metadata['pmd:timeHorizon'] = ""
-
-    elif len(meta_list) == 5:
-
-        file_metadata['pmd:validFrom'], file_metadata['pmd:timeHorizon'], model_authority, file_metadata['pmd:cgmesProfile'], file_metadata['pmd:versionNumber'] = meta_list
-
-    else:
-        print("Parsing error, number of allowed meta in filename is 4 or 5 separated by '_' -> {} ".format(file_name))
-
-    model_authority_list = model_authority.split("-")
-
-    if len(model_authority_list) == 1:
-        file_metadata['pmd:modelPartReference'] = model_authority
-
-    elif len(model_authority_list) == 2:
-        file_metadata['pmd:mergingEntity'], file_metadata['pmd:mergingArea'] = model_authority_list
-
-    elif len(model_authority_list) == 3:
-        file_metadata['pmd:mergingEntity'], file_metadata['pmd:mergingArea'], file_metadata['pmd:modelPartReference'] = model_authority_list
-
-    else:
-        print(f"Parsing error {model_authority}")
-
-    return file_metadata
-
-
-def filename_from_metadata(metadata):
-
-    model_part = metadata.get('pmd:modelPartReference', None)
-
-    if model_part:
-        model_authority = f"{metadata['pmd:mergingEntity']}-{metadata['pmd:mergingArea']}-{model_part}"
-
-    else:
-        model_authority = f"{metadata['pmd:mergingEntity']}-{metadata['pmd:mergingArea']}"
-
-    file_name = f"{metadata['pmd:validFrom']}_{metadata['pmd:timeHorizon']}_{model_authority}_{metadata['pmd:cgmesProfile']}_{metadata['pmd:versionNumber']}"
-    file_name = ".".join([file_name, metadata["file_type"]])
-
-    return file_name
-
-
-def get_xml_from_zip(zip_file_path):
-
-    zipfile_object    = zipfile.ZipFile(zip_file_path)
-    xml_file_name     = zipfile_object.namelist()[0]
-    file_unzipped     = zipfile_object.open(xml_file_name, mode="r")
-    xml_tree_object   = etree.parse(file_unzipped)
-
-    return xml_tree_object
-
-def zip_xml_file(xml_etree_object, file_metadata, destination_bath):
-
-    # Get meta and path
-    file_metadata["file_type"] = "zip"
-    zip_file_name = filename_from_metadata(file_metadata)
-
-    file_metadata["file_type"] = "xml"
-    xml_file_name = filename_from_metadata(file_metadata)
-
-    zip_file_path = os.path.join(destination_bath, zip_file_name)
-
-    # Create and save ZIP
-    out_zipped_file = zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED)
-    out_zipped_file.writestr(xml_file_name, etree.tostring(xml_etree_object))#, pretty_print=True))
-    out_zipped_file.close()
-
-    return zip_file_path
-
-
-def get_metadata_from_xml(parsed_xml):
-    """Parse model metadata form xml, retruns a dictionary"""
-    #parsed_xml = etree.parse(filepath_or_fileobject)
-
-    header = parsed_xml.find("{*}FullModel")
-    meta_elements = header.getchildren()
-
-    # Add model ID
-    meta_dict = {"mRID":header.attrib.values()[0].split(":")[-1]}
-
-    # Add all other metadata
-    for element in meta_elements:
-        if element.text:
-            meta_dict[element.tag.split("}")[1]] = element.text
-        else:
-            meta_dict[element.tag.split("}")[1]] = element.attrib.values()[0]
-
-    return meta_dict
-
-
-def get_metadata_from_filename(file_name):
-
-    # Separators
-    file_type_separator           = "."
-    meta_separator                = "_"
-    entity_and_domain_separator   = "-"
-
-    #print(file_name)
-    file_metadata = {}
-    file_name, file_type = file_name.split(file_type_separator)
-
-    # Parse file metadata
-    file_meta_list = file_name.split(meta_separator)
-
-    # Naming before QoDC 2.1, where EQ might not have processType
-    if len(file_meta_list) == 4:
-
-        file_metadata["Model.scenarioTime"],\
-        file_metadata["Model.modelingEntity"],\
-        file_metadata["Model.messageType"],\
-        file_metadata["Model.version"] = file_meta_list
-        file_metadata["Model.processType"] = ""
-
-        print("Warning - only 4 meta elements found, expecting 5, setting Model.processType to empty string")
-
-    # Naming after QoDC 2.1, always 5 positions
-    elif len(file_meta_list) == 5:
-
-        file_metadata["Model.scenarioTime"],\
-        file_metadata["Model.processType"],\
-        file_metadata["Model.modelingEntity"],\
-        file_metadata["Model.messageType"],\
-        file_metadata["Model.version"] = file_meta_list
-
-    else:
-        print("Non CGMES file {}".format(file_name))
-
-    if file_metadata.get("Model.modelingEntity", False):
-
-        entity_and_area_list = file_metadata["Model.modelingEntity"].split(entity_and_domain_separator)
-
-        if len(entity_and_area_list) == 1:
-            file_metadata["Model.mergingEntity"],\
-            file_metadata["Model.domain"] = "", "" # Set empty string for both
-            file_metadata["Model.forEntity"] = entity_and_area_list[0]
-
-        if len(entity_and_area_list) == 2:
-            file_metadata["Model.mergingEntity"],\
-            file_metadata["Model.domain"] = entity_and_area_list
-            file_metadata["Model.forEntity"] = ""
-
-        if len(entity_and_area_list) == 3:
-            file_metadata["Model.mergingEntity"],\
-            file_metadata["Model.domain"],\
-            file_metadata["Model.forEntity"] = entity_and_area_list
-
-    return file_metadata
 
 opdm_client = OPDM()
 
@@ -229,18 +56,6 @@ for model in available_models:
 
 # Remove all available models to save memory
 del available_models
-
-export_settings = {
-    "iidm.export.cgmes.base-name": "",
-    "iidm.export.cgmes.cim-version": "",  # 14, 16, 100
-    "iidm.export.cgmes.export-boundary-power-flows": "true",
-    "iidm.export.cgmes.export-power-flows-for-switches": "true",
-    "iidm.export.cgmes.naming-strategy": "identity",  # identity, cgmes, cgmes-fix-all-invalid-ids
-    "iidm.export.cgmes.profiles": "EQ,TP,SSH,SV",
-    "iidm.export.cgmes.boundary-EQ-identifier": "",
-    "iidm.export.cgmes.boundary-TP-identifier": "",
-    "iidm.export.cgmes.modeling-authority-set": "powsybl.org"
-}
 
 merged_model = load_model(valid_models + [latest_boundary])
 
@@ -446,6 +261,7 @@ export = pandas.concat([updated_ssh_data, sv_data], ignore_index=True).export_to
 
 publication_responses = []
 for instance_file in export:
+    logger.info(f"Publishing {instance_file.name} to OPDM")
     publication_response = opdm_client.publication_request(instance_file, "CGMES")
     publication_responses.append(
         {"name": instance_file.name,
