b6743eb6b69afba5325b2e466f25c0e951c82ebf
martynas.karobcikas <mkarobcikas@gmail.com>
2024-01-04 09:45:24 +0200

    pipenv: updated EDX to latest version feature: enabled id from meta for schedule worker

--- emf/schedule_retriever/worker.py
+++ emf/schedule_retriever/worker.py
@@ -14,7 +14,7 @@ parse_app_properties(caller_globals=globals(), path=config.paths.schedule_retrie
 
 # Transfer schedules from EDX to Elk
 message_types = EDX_MESSAGE_TYPE.split(",")
-elk_handler = elastic.Handler(index=ELK_INDEX_PATTERN)
+elk_handler = elastic.Handler(index=ELK_INDEX_PATTERN, id_from_metadata=True)
 service = edx.EDX(converter=iec_schedule_to_ndjson, handler=elk_handler, message_types=message_types)
 service.run()
 
