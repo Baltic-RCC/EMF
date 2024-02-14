848c56fa1197e6ce258432cd54fdb3283b4f5b2b
gintautas.poderys <gintautasp25@gmail.com>
2024-02-14 16:51:51 +0200

    Added validator to model retriever

--- emf/model_retriever/worker.py
+++ emf/model_retriever/worker.py
@@ -7,6 +7,8 @@ from emf.model_retriever import model_retriever
 from emf.common.integrations import elastic, opdm, minio, edx
 from emf.common.logging import custom_logger
 from emf.common.config_parser import parse_app_properties
+from emf.loadflow_tool.validator import validate_model
+
 
 # Initialize custom logger
 custom_logger.initialize_custom_logger(extra={'worker': 'model-retriever', 'worker_uuid': str(uuid.uuid4())})
@@ -17,6 +19,7 @@ parse_app_properties(caller_globals=globals(), path=config.paths.model_retriever
 edx_service = edx.EDX()
 opdm_service = opdm.OPDM()
 minio_service = minio.ObjectStorage()
+
 elk_service = elastic.Handler(index=ELK_INDEX_PATTERN, id_from_metadata=True, id_metadata_list=['opde:Id'])
 
 while True:
@@ -31,13 +34,19 @@ while True:
     opdm_objects = json.loads(body)
     opdm_objects = model_retriever.opde_models_to_minio(opdm_objects=opdm_objects, opdm_service=opdm_service, minio_service=minio_service)
 
-    # TODO Validate model
+    # Get latest boundary set for validation
+    latest_boundary = opdm_service.get_latest_boundary()
 
+    # Run validation
     for opdm_object in opdm_objects:
-        # Removing content from opdm_object to only keep the model metadata
+        response = validate_model([opdm_object, latest_boundary])
+
+        # Filter out non metadata
+        response.pop('NETWORK')
+        opdm_object["VALIDATION_STATUS"] = response
         for component in opdm_object['opde:Component']:
             component['opdm:Profile'].pop('DATA')
 
     # Send model metadata to ELK
-    elk_service.send(byte_string=json.dumps(opdm_objects).encode('utf-8'), properties=properties)
+    elk_service.send(byte_string=json.dumps(opdm_objects, default=str).encode('utf-8'), properties=properties)
     logger.info(f"Model metadata sent to ELK")
