2ae75cbf2492d7af389b843076085f2cdcfa5a73
gintautas.poderys <gintautasp25@gmail.com>
2024-02-20 15:29:26 +0200

    [#38] Updated XSLT to export byte type objects

--- emf/common/xslt_engine/saxonpy_api.py
+++ emf/common/xslt_engine/saxonpy_api.py
@@ -7,36 +7,47 @@ import sys
 logger = logging.getLogger(__name__)
 
 
-def xslt30_convert(source_file, stylesheet_file, output_file, debug=False):
+def xslt30_convert(source_file, stylesheet_file, output_file=None):
     with PySaxonProcessor() as saxon:
         xslt30 = saxon.new_xslt30_processor()
-        executable = xslt30.compile_stylesheet(stylesheet_file=stylesheet_file)
 
         if str == type(source_file):
             document = saxon.parse_xml(xml_file_name=source_file)
-
         if bytes == type(source_file):
             document = saxon.parse_xml(xml_text=source_file.decode("utf-8"))
 
-        if not debug:
-            output_file = os.path.abspath(f'../../../config/report_publisher/{output_file}')
+        if str == type(stylesheet_file):
+            executable = xslt30.compile_stylesheet(stylesheet_file=stylesheet_file)
+        if bytes == type(stylesheet_file):
+            executable = xslt30.compile_stylesheet(stylesheet_text=stylesheet_file.decode("utf-8"))
 
-        executable.transform_to_file(xdm_node=document, output_file=output_file)
-        logger.info(f"XML transform completed, output file -> {output_file}")
+        if output_file:
+            logger.info(f"XML transform completed, output file -> {output_file}")
+            executable.transform_to_file(xdm_node=document, output_file=output_file)
+
+    return executable.transform_to_string(xdm_node=document).encode("utf-8")
 
 
 def validate_xml(input_xml, schema_xml):
-    logger.info(f"Validating {input_xml} against {schema_xml}")
-    document = etree.parse(input_xml)
-    schema = etree.XMLSchema(etree.parse(schema_xml))
+
+    logger.info(f"Validating XML against XSD")
+    if str == type(input_xml):
+        document = etree.parse(input_xml)
+    if bytes == type(input_xml):
+        document = etree.fromstring(input_xml)
+
+    if str == type(schema_xml):
+        schema = etree.XMLSchema(etree.parse(schema_xml))
+    if bytes == type(schema_xml):
+        schema = etree.XMLSchema(etree.fromstring(schema_xml))
+
     is_valid = schema.validate(document)
     for error in schema.error_log:
         logger.error(error)
-
     if is_valid:
-        logger.info(f"File is valid -> {input_xml}")
+        logger.info(f"XML file is valid")
     if not is_valid:
-        logger.error(f"File is invalid -> {input_xml}")
+        logger.error(f"XML file is invalid")
 
     return is_valid
 
@@ -54,14 +65,17 @@ if __name__ == '__main__':
     test_output_file = "test_results.xml"
     test_output_file2 = "test_results2.xml"
 
+    with open('IGM_entsoeQAReport_Level_8.xsl', 'rb') as file:
+        xsl_bytes = file.read()
     with open('failed_igm_report.xml', 'rb') as file:
         xml_bytes = file.read()
+    with open('QAR_v2.6.1.xsd', 'rb') as file:
+        xsd_bytes = file.read()
 
     # test with file path and bytes object
-    xslt30_convert(test_file, test_stylesheet_file, test_output_file, debug=True)
-    xslt30_convert(xml_bytes, test_stylesheet_file, test_output_file2, debug=True)
-
+    result = xslt30_convert(test_file, test_stylesheet_file)
+    result2 = xslt30_convert(xml_bytes, xsl_bytes)
     validate_xml(test_output_file, xsd_file)
-    validate_xml(test_output_file2, xsd_file)
+    validate_xml(result2, xsd_bytes)
 
-    print('Test script finished')
+    print('Script finished')
