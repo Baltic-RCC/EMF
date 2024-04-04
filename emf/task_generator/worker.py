79f6b3d4e727b3c05fa7a22453996ddca5ccf453
gintautas.poderys <gintautasp25@gmail.com>
2024-04-04 09:59:39 +0300

    #27 fix: config file pathing

--- emf/task_generator/worker.py
+++ emf/task_generator/worker.py
@@ -2,9 +2,11 @@ import json
 import config
 import logging
 import sys
+from pathlib import Path
 from emf.task_generator.task_generator import generate_tasks
 from emf.common.integrations import rabbit
 from emf.common.config_parser import parse_app_properties
+
 
 logging.basicConfig(stream=sys.stdout,
                     format="%(levelname) -10s %(asctime) -10s %(name) -35s %(funcName) -30s %(lineno) -5d: %(message)s",
@@ -14,7 +16,10 @@ logger = logging.getLogger()
 
 parse_app_properties(globals(), config.paths.task_generator.task_generator)
 
-tasks = list(generate_tasks(TASK_WINDOW_DURATION, TASK_WINDOW_REFERENCE, PROCESS_CONF, TIMEFRAME_CONF))
+timeframe_conf = str(Path(__file__).parent.parent.parent.joinpath('config/task_generator/timeframe_conf.json'))
+process_conf = str(Path(__file__).parent.parent.parent.joinpath('config/task_generator/process_conf.json'))
+
+tasks = list(generate_tasks(TASK_WINDOW_DURATION, TASK_WINDOW_REFERENCE, process_conf, timeframe_conf))
 
 if tasks:
     rabbit_service = rabbit.BlockingClient()
