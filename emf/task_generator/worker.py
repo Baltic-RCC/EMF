e470028079a43e4f77a8cd2f49ae4f7599f5adb3
Kristjan <kristjan.vilgo@elering.ee>
2024-05-22 11:39:44 +0300

    Small updates to Task worker.py

--- emf/task_generator/worker.py
+++ emf/task_generator/worker.py
@@ -14,9 +14,10 @@ parse_app_properties(globals(), config.paths.task_generator.task_generator)
 timeframe_conf = config.paths.task_generator.timeframe_conf
 process_conf = config.paths.task_generator.process_conf
 
-tasks = generate_tasks(TASK_WINDOW_DURATION, TASK_WINDOW_REFERENCE, process_conf, timeframe_conf)
+tasks = list(generate_tasks(TASK_WINDOW_DURATION, TASK_WINDOW_REFERENCE, process_conf, timeframe_conf))
 
 if tasks:
+    logger.info(f"Creating connection to RMQ")
     rabbit_service = rabbit.BlockingClient()
     logger.info(f"Sending tasks to Rabbit exchange '{RMQ_EXCHANGE}'")
     for task in tasks:
