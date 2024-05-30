178ff8b87d52c24edcdd3fd9a3be337e6c937e0e
Vilgo <kristjan.vilgo@elering.ee>
2024-05-30 21:03:19 +0300

    Updated RMM handler
    #66

--- emf/loadflow_tool/handlers/rmm_handler.py
+++ emf/loadflow_tool/handlers/rmm_handler.py
@@ -1,52 +1,20 @@
 import logging
 import config
+import json
+from uuid import uuid4
+import datetime
+from aniso8601 import parse_datetime
 from io import BytesIO
 from zipfile import ZipFile
-from typing import List
-import json
-
 from emf.common.config_parser import parse_app_properties
-from emf.common.integrations import edx, elastic, opdm, minio
-from emf.loadflow_tool.merger import *
+from emf.common.integrations import edx, opdm, minio
+from emf.common.integrations.object_storage.models import get_latest_boundary, get_latest_models_and_download
+from emf.loadflow_tool import loadflow_settings
+from emf.loadflow_tool.helper import metadata_from_filename
+from emf.loadflow_tool.merger import filter_models, fix_sv_tapsteps, fix_sv_shunts, load_model, run_lf, create_sv_and_updated_ssh, export_to_cgmes_zip
 
 logger = logging.getLogger(__name__)
-parse_app_properties(caller_globals=globals(), path=config.paths.model_retriever.model_retriever)
-
-sample_task = {
-    "@context": "https://example.com/task_context.jsonld",
-    "@type": "Task",
-    "@id": "urn:uuid:ee3c57bf-fa4e-402c-82ac-7352c0d8e118",
-    "process_id": "https://example.com/processes/CGM_CREATION",
-    "run_id": "https://example.com/runs/IntraDayCGM/1",
-    "job_id": "urn:uuid:d9343f48-23cd-4d8a-ae69-1940a0ab1837",
-    "task_type": "automatic",
-    "task_initiator": "teenus.testrscjslv1",
-    "task_priority": "normal",
-    "task_creation_time": "2024-05-28T20:39:42.448064",
-    "task_status": "created",
-    "task_status_trace": [
-        {
-            "status": "created",
-            "timestamp": "2024-05-28T20:39:42.448064"
-        }
-    ],
-    "task_dependencies": [],
-    "task_tags": [],
-    "task_retry_count": 0,
-    "task_timeout": "PT1H",
-    "task_gate_open": "2024-05-24T21:00:00+00:00",
-    "task_gate_close": "2024-05-24T21:15:00+00:00",
-    "job_period_start": "2024-05-24T22:00:00+00:00",
-    "job_period_end": "2024-05-25T06:00:00+00:00",
-    "task_properties": {
-        "timestamp_utc": "2024-05-25T02:30:00+00:00",
-        "merge_type": "EU",
-        "included": [],
-        "excluded": [],
-        "time_horizon": "1D"
-    }
-}
-
+parse_app_properties(caller_globals=globals(), path=config.paths.cgm_worker.merger)
 
 
 class HandlerRmmToPdnAndMinio:
@@ -54,14 +22,17 @@ class HandlerRmmToPdnAndMinio:
     def __init__(self):
         self.opdm_service = opdm.OPDM()
         self.minio_service = minio.ObjectStorage()
-        self.edx_service = edx.create_client()
+        self.edx_service = edx.EDX()
 
     def handle(self, task_object: dict, **kwargs):
 
         start_time = datetime.datetime.utcnow()
 
         # Parse relevant data from Task
-        task = json.loads(task_object)
+        task = task_object
+
+        if isinstance(task, str):
+            task = json.loads(task_object)
 
         task["task_status_trace"].append(
             {
@@ -80,29 +51,27 @@ class HandlerRmmToPdnAndMinio:
         merging_area = task_properties["merge_type"]
 
         # TODO add to task
-        merging_entity = "BALTICRSC"
-        mas = 'http://www.baltic-rsc.eu/OperationalPlanning/RMM'
+        merging_entity = task_properties["merging_entity"]
+        mas = task_properties["mas"]
 
         # TODO - task to contain and increase version number
-        version = "104"
+        version = task_properties["version"]
 
         # Set RMM name
         rmm_name = f"RMM_{time_horizon}_{version}_{parse_datetime(scenario_datetime):%Y%m%dT%H%MZ}_{merging_area}_{uuid4()}"
 
         # Collect models
-        valid_models = get_latest_models_and_download(time_horizon, scenario_date, valid=True)
+        valid_models = get_latest_models_and_download(time_horizon, scenario_datetime, valid=True)
         latest_boundary = get_latest_boundary()
 
         filtered_models = filter_models(valid_models, included_models, excluded_models)
 
         # Get additional models
-
-
         if time_horizon == 'ID':
             # takes any integer between 0-32 which can be in model name
-            model_name_pattern = f"{parse_datetime(scenario_date):%Y%m%dT%H%M}Z-({'0[0-9]|1[0-9]|2[0-9]|3[0-6]'})-({'|'.join(local_import_models)})"
+            model_name_pattern = f"{parse_datetime(scenario_datetime):%Y%m%dT%H%M}Z-({'0[0-9]|1[0-9]|2[0-9]|3[0-6]'})-({'|'.join(local_import_models)})"
         else:
-            model_name_pattern = f"{parse_datetime(scenario_date):%Y%m%dT%H%M}Z-{time_horizon}-({'|'.join(local_import_models)})"
+            model_name_pattern = f"{parse_datetime(scenario_datetime):%Y%m%dT%H%M}Z-{time_horizon}-({'|'.join(local_import_models)})"
         additional_model_metadata = {'bamessageid': model_name_pattern}
 
         additional_models = self.minio_service.query_objects(
@@ -141,9 +110,9 @@ class HandlerRmmToPdnAndMinio:
                     'opde:Component': []
                 }
 
-                logger.info(f"Loading additional model {model.name}", extra={"additional_model_name": model.name})
-                model_data = BytesIO(self.minio_service.download_object(model.object_name))
-                model_data.name = f"{model.name}.zip"
+                logger.info(f"Loading additional model {model.object_name}", extra={"additional_model_name": model.object_name})
+                model_data = BytesIO(self.minio_service.download_object(bucket_name=INPUT_MINIO_BUCKET, object_name=model.object_name))
+                model_data.name = f"{model.metadata.get('X-Amz-Meta-Bamessageid')}.zip"
 
                 with ZipFile(model_data) as source_zip:
 
@@ -155,15 +124,14 @@ class HandlerRmmToPdnAndMinio:
                         with ZipFile(data, "w") as data_zip:
                             data_zip.writestr(file_name, source_zip.open(file_name).read())
 
-                        opdm_object['opde:Component'].append(
-                            {
-                                'opdm:Profile':
-                                {
-                                    "pmd:content-reference": data.name,
-                                    "DATA": data.getvalue(),
-                                }
-                            }
-                        )
+                        metadata = {"pmd:content-reference": data.name,
+                                    "pmd:fileName": data.name,
+                                    "DATA": data.getvalue()}
+
+                        metadata.update(metadata_from_filename(data.name))
+                        opdm_profile = {'opdm:Profile': metadata}
+                        opdm_object['opde:Component'].append(opdm_profile)
+
 
                 additional_models_data.append(opdm_object)
         else:
@@ -177,8 +145,7 @@ class HandlerRmmToPdnAndMinio:
         solved_model = run_lf(merged_model, loadflow_settings=loadflow_settings.CGM_DEFAULT)
 
         # TODO - get version dynamically form ELK
-        sv_data, ssh_data = create_sv_and_updated_ssh(solved_model, valid_models, time_horizon, version, merging_area,
-                                                      merging_entity, mas)
+        sv_data, ssh_data = create_sv_and_updated_ssh(solved_model, input_models, scenario_datetime, time_horizon, version, merging_area, merging_entity, mas)
 
         # Fix SV
         sv_data = fix_sv_shunts(sv_data, valid_models)
@@ -192,7 +159,7 @@ class HandlerRmmToPdnAndMinio:
 
             # Include CGM model files
             for item in serialized_data:
-                rmm_zip.writestr(item.file_name, item.getvalue())
+                rmm_zip.writestr(item.name, item.getvalue())
 
             # Include original IGM files
             for object in input_models:
@@ -205,8 +172,8 @@ class HandlerRmmToPdnAndMinio:
 
 
         # Upload to PDN
-        business_type = OUTPUT_EDX_RECEIVER     # "MODELS-RMM"
-        receiver = OUTPUT_EDX_BUSINESS_TYPE     # "PUBLICATION@SC-38V-0000000012-W"
+        business_type = OUTPUT_EDX_BUSINESS_TYPE # "MODELS-RMM"
+        receiver = OUTPUT_EDX_RECEIVER # "PUBLICATION@SC-38V-0000000012-W"
 
         logger.info(f"Publishing {business_type} to {receiver}")
         try:
@@ -226,13 +193,13 @@ class HandlerRmmToPdnAndMinio:
         logger.info(f"Uploading RMM to MINO {OUTPUT_MINIO_BUCKET}/{rmm_object.name}")
         try:
 
-            self.minio_service.upload_object(rmm_object)
+            self.minio_service.upload_object(rmm_object, bucket_name=OUTPUT_MINIO_BUCKET)
 
         except:
             logging.error(f"""Unexpected error on uploading to Object Storage:""", exc_info=True)
 
         logger.info(f"RMM creation done for {rmm_name}")
-        end_time = datetime.now()
+        end_time = datetime.datetime.now()
         task_duration = end_time - start_time
         logger.info(f"Task ended at {end_time}, total run time {task_duration}",
                     extra={"task_duration": task_duration.total_seconds(),
@@ -242,10 +209,63 @@ class HandlerRmmToPdnAndMinio:
         task["task_status_trace"].append(
             {
                 "status": "task_finished",
-                "timestamp": datetime.utcnow().isoformat()
+                "timestamp": datetime.datetime.utcnow().isoformat()
             }
         )
 
         logger.debug(task)
 
         return task
+
+
+if __name__ == "__main__":
+
+    import sys
+    logging.basicConfig(
+        format='%(levelname)-10s %(asctime)s.%(msecs)03d %(name)-30s %(funcName)-35s %(lineno)-5d: %(message)s',
+        datefmt='%Y-%m-%dT%H:%M:%S',
+        level=logging.INFO,
+        handlers=[logging.StreamHandler(sys.stdout)]
+    )
+
+    sample_task = {
+        "@context": "https://example.com/task_context.jsonld",
+        "@type": "Task",
+        "@id": "urn:uuid:ee3c57bf-fa4e-402c-82ac-7352c0d8e118",
+        "process_id": "https://example.com/processes/CGM_CREATION",
+        "run_id": "https://example.com/runs/IntraDayCGM/1",
+        "job_id": "urn:uuid:d9343f48-23cd-4d8a-ae69-1940a0ab1837",
+        "task_type": "automatic",
+        "task_initiator": "teenus.testrscjslv1",
+        "task_priority": "normal",
+        "task_creation_time": "2024-05-28T20:39:42.448064",
+        "task_status": "created",
+        "task_status_trace": [
+            {
+                "status": "created",
+                "timestamp": "2024-05-28T20:39:42.448064"
+            }
+        ],
+        "task_dependencies": [],
+        "task_tags": [],
+        "task_retry_count": 0,
+        "task_timeout": "PT1H",
+        "task_gate_open": "2024-05-24T21:00:00+00:00",
+        "task_gate_close": "2024-05-24T21:15:00+00:00",
+        "job_period_start": "2024-05-24T22:00:00+00:00",
+        "job_period_end": "2024-05-25T06:00:00+00:00",
+        "task_properties": {
+            "timestamp_utc": "2024-05-22T11:30:00+00:00",
+            "merge_type": "EU",
+            "merging_entity": "BALTICRSC",
+            "included": ["ELERING", "AST", "PSE"],
+            "excluded": [],
+            "local_import": ["LITGRID"],
+            "time_horizon": "1D",
+            "version": "104",
+            "mas": "http://www.baltic-rsc.eu/OperationalPlanning/RMM"
+        }
+    }
+
+    worker = HandlerRmmToPdnAndMinio()
+    finished_task = worker.handle(sample_task)
