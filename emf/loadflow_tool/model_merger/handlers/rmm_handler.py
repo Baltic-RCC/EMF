48dded100dd180a38ce0bfa1e5607ef70c9fb77d
VeikoAunapuu <veikoaunapuu12@gmail.com>
2024-08-12 13:25:00 +0300

    Process check if found any models to merge

--- emf/loadflow_tool/model_merger/handlers/rmm_handler.py
+++ emf/loadflow_tool/model_merger/handlers/rmm_handler.py
@@ -112,96 +112,103 @@ class HandlerRmmToPdnAndMinio:
 
         # Get additional models directly from Minio
         additional_models_data = self.minio_service.get_latest_models_and_download(time_horizon, scenario_datetime, local_import_models, bucket_name=INPUT_MINIO_BUCKET, prefix=INPUT_MINIO_FOLDER)
+        
+        input_models = filtered_models + additional_models_data
+        
+        #Run Process only if you find some models to merge, otherwise return None
+        if not input_models:
+            logger.warning("Found no Models To Merge, Returning NONE")
+            return None
+        else:
+            # Load all selected models
+            input_models = input_models + [latest_boundary]
+            # SET BRELL LINE VALUES
+            input_models = set_brell_lines_to_zero_in_models(input_models)
 
-        # Load all selected models
-        input_models = filtered_models + additional_models_data + [latest_boundary]
-        # SET BRELL LINE VALUES
-        input_models = set_brell_lines_to_zero_in_models(input_models)
+            assembeled_data = merge_functions.load_opdm_data(input_models)
+            assembeled_data = triplets.cgmes_tools.update_FullModel_from_filename(assembeled_data)
+            assembeled_data = merge_functions.configure_paired_boundarypoint_injections(assembeled_data)
+            # END OF MODIFICATION
+            input_models = create_opdm_objects([merge_functions.export_to_cgmes_zip([assembeled_data])])
+            del assembeled_data
 
-        assembeled_data = merge_functions.load_opdm_data(input_models)
-        assembeled_data = triplets.cgmes_tools.update_FullModel_from_filename(assembeled_data)
-        assembeled_data = merge_functions.configure_paired_boundarypoint_injections(assembeled_data)
-        # END OF MODIFICATION
-        input_models = create_opdm_objects([merge_functions.export_to_cgmes_zip([assembeled_data])])
-        del assembeled_data
+            merged_model = merge_functions.load_model(input_models)
 
-        merged_model = merge_functions.load_model(input_models)
+            # TODO - run other LF if default fails
+            solved_model = merge_functions.run_lf(merged_model, loadflow_settings=getattr(loadflow_settings, MERGE_LOAD_FLOW_SETTINGS))
 
-        # TODO - run other LF if default fails
-        solved_model = merge_functions.run_lf(merged_model, loadflow_settings=getattr(loadflow_settings, MERGE_LOAD_FLOW_SETTINGS))
+            # Update time_horizon in case of generic ID process type
+            if time_horizon.upper() == "ID":
+                _task_creation_time = parse_datetime(task_creation_time, keep_timezone=False)
+                _scenario_datetime = parse_datetime(scenario_datetime, keep_timezone=False)
 
-        # Update time_horizon in case of generic ID process type
-        if time_horizon.upper() == "ID":
-            _task_creation_time = parse_datetime(task_creation_time, keep_timezone=False)
-            _scenario_datetime = parse_datetime(scenario_datetime, keep_timezone=False)
+                time_horizon = f"{int((_scenario_datetime - _task_creation_time).seconds/3600):02d}"
+                logger.info(f"Setting ID TimeHorizon to {time_horizon}")
 
-            time_horizon = f"{int((_scenario_datetime - _task_creation_time).seconds/3600):02d}"
-            logger.info(f"Setting ID TimeHorizon to {time_horizon}")
+            # TODO - get version dynamically form ELK
+            sv_data, ssh_data = merge_functions.create_sv_and_updated_ssh(solved_model, input_models, scenario_datetime, time_horizon, version, merging_area, merging_entity, mas)
 
-        # TODO - get version dynamically form ELK
-        sv_data, ssh_data = merge_functions.create_sv_and_updated_ssh(solved_model, input_models, scenario_datetime, time_horizon, version, merging_area, merging_entity, mas)
+            # Fix SV
+            sv_data = merge_functions.fix_sv_shunts(sv_data, input_models)
+            sv_data = merge_functions.fix_sv_tapsteps(sv_data, ssh_data)
+            sv_data = merge_functions.remove_small_islands(sv_data, int(SMALL_ISLAND_SIZE))
+            models_as_triplets = merge_functions.load_opdm_data(input_models)
+            sv_data = merge_functions.remove_duplicate_sv_voltages(cgm_sv_data=sv_data,
+                                                                original_data=models_as_triplets)
+            sv_data = merge_functions.check_and_fix_dependencies(cgm_sv_data=sv_data,
+                                                                cgm_ssh_data=ssh_data,
+                                                                original_data=models_as_triplets)
+            sv_data, ssh_data = merge_functions.disconnect_equipment_if_flow_sum_not_zero(cgm_sv_data=sv_data,
+                                                                                        cgm_ssh_data=ssh_data,
+                                                                                        original_data=models_as_triplets)
+            # Package both input models and exported CGM profiles to in memory zip files
+            serialized_data = merge_functions.export_to_cgmes_zip([ssh_data, sv_data])
 
-        # Fix SV
-        sv_data = merge_functions.fix_sv_shunts(sv_data, input_models)
-        sv_data = merge_functions.fix_sv_tapsteps(sv_data, ssh_data)
-        sv_data = merge_functions.remove_small_islands(sv_data, int(SMALL_ISLAND_SIZE))
-        models_as_triplets = merge_functions.load_opdm_data(input_models)
-        sv_data = merge_functions.remove_duplicate_sv_voltages(cgm_sv_data=sv_data,
-                                                               original_data=models_as_triplets)
-        sv_data = merge_functions.check_and_fix_dependencies(cgm_sv_data=sv_data,
-                                                             cgm_ssh_data=ssh_data,
-                                                             original_data=models_as_triplets)
-        sv_data, ssh_data = merge_functions.disconnect_equipment_if_flow_sum_not_zero(cgm_sv_data=sv_data,
-                                                                                      cgm_ssh_data=ssh_data,
-                                                                                      original_data=models_as_triplets)
-        # Package both input models and exported CGM profiles to in memory zip files
-        serialized_data = merge_functions.export_to_cgmes_zip([ssh_data, sv_data])
+            # Set RMM name
+            rmm_name = f"RMM_{time_horizon}_{version}_{parse_datetime(scenario_datetime):%Y%m%dT%H%MZ}_{merging_area}_{uuid4()}"
 
-        # Set RMM name
-        rmm_name = f"RMM_{time_horizon}_{version}_{parse_datetime(scenario_datetime):%Y%m%dT%H%MZ}_{merging_area}_{uuid4()}"
+            rmm_data = BytesIO()
+            with ZipFile(rmm_data, "w") as rmm_zip:
 
-        rmm_data = BytesIO()
-        with ZipFile(rmm_data, "w") as rmm_zip:
+                # Include CGM model files
+                for item in serialized_data:
+                    rmm_zip.writestr(item.name, item.getvalue())
 
-            # Include CGM model files
-            for item in serialized_data:
-                rmm_zip.writestr(item.name, item.getvalue())
+                # Include original IGM files
+                for object in input_models:
+                    for instance in object['opde:Component']:
+                        with ZipFile(BytesIO(instance['opdm:Profile']['DATA'])) as instance_zip:
+                            for file_name in instance_zip.namelist():
+                                logging.info(f"Adding file: {file_name}")
+                                rmm_zip.writestr(file_name, instance_zip.open(file_name).read())
 
-            # Include original IGM files
-            for object in input_models:
-                for instance in object['opde:Component']:
-                    with ZipFile(BytesIO(instance['opdm:Profile']['DATA'])) as instance_zip:
-                        for file_name in instance_zip.namelist():
-                            logging.info(f"Adding file: {file_name}")
-                            rmm_zip.writestr(file_name, instance_zip.open(file_name).read())
+            # Upload to Object Storage
+            rmm_object = rmm_data
+            rmm_object.name = f"{OUTPUT_MINIO_FOLDER}/{rmm_name}.zip"
+            logger.info(f"Uploading RMM to MINO {OUTPUT_MINIO_BUCKET}/{rmm_object.name}")
 
-        # Upload to Object Storage
-        rmm_object = rmm_data
-        rmm_object.name = f"{OUTPUT_MINIO_FOLDER}/{rmm_name}.zip"
-        logger.info(f"Uploading RMM to MINO {OUTPUT_MINIO_BUCKET}/{rmm_object.name}")
+            try:
+                self.minio_service.upload_object(rmm_object, bucket_name=OUTPUT_MINIO_BUCKET)
+            except:
+                logging.error(f"""Unexpected error on uploading to Object Storage:""", exc_info=True)
 
-        try:
-            self.minio_service.upload_object(rmm_object, bucket_name=OUTPUT_MINIO_BUCKET)
-        except:
-            logging.error(f"""Unexpected error on uploading to Object Storage:""", exc_info=True)
+            logger.info(f"RMM creation done for {rmm_name}")
+            end_time = datetime.datetime.utcnow()
+            task_duration = end_time - start_time
+            logger.info(f"Task ended at {end_time}, total run time {task_duration}",
+                        extra={"task_duration": task_duration.total_seconds(),
+                            "task_start_time": start_time.isoformat(),
+                            "task_end_time": end_time.isoformat()})
 
-        logger.info(f"RMM creation done for {rmm_name}")
-        end_time = datetime.datetime.utcnow()
-        task_duration = end_time - start_time
-        logger.info(f"Task ended at {end_time}, total run time {task_duration}",
-                    extra={"task_duration": task_duration.total_seconds(),
-                           "task_start_time": start_time.isoformat(),
-                           "task_end_time": end_time.isoformat()})
+            # Set task to started
+            update_task_status(task, "finished")
 
-        # Set task to started
-        update_task_status(task, "finished")
+            logger.debug(task)
 
-        logger.debug(task)
+            # Stop Trace
+            self.elk_logging_handler.stop_trace()
 
-        # Stop Trace
-        self.elk_logging_handler.stop_trace()
-
-        return task
+            return task
 
 
 if __name__ == "__main__":
