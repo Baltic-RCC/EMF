d7a23a00fd7739789a7d9e85698e14de7f4eaa4d
martynas.karobcikas <martynas.karobcikas@baltic-rcc.eu>
2024-10-08 10:31:04 +0300

    logging: disabled triplet library logging to WARNING level

--- emf/loadflow_tool/model_merger/merge_worker.py
+++ emf/loadflow_tool/model_merger/merge_worker.py
@@ -8,11 +8,12 @@ from emf.loadflow_tool.model_merger.merge_handler import HandlerMergeModels
 
 # Initialize custom logger
 logger = logging.getLogger(__name__)
-elk_handler = custom_logger.initialize_custom_logger(extra={'worker': 'rmm-merger', 'worker_uuid': str(uuid4())})
+elk_handler = custom_logger.initialize_custom_logger(extra={'worker': 'model-merger', 'worker_uuid': str(uuid4())})
+
+# Disabling triplets library logging at INFO level
+logging.getLogger('triplets').setLevel(logging.WARNING)
 
 parse_app_properties(caller_globals=globals(), path=config.paths.cgm_worker.merger)
-
-logger.info(f"Merge load flow parameters used used: {MERGE_LOAD_FLOW_SETTINGS}")
 
 # RabbitMQ consumer implementation
 consumer = rabbit.RMQConsumer(que=INPUT_RABBIT_QUE, message_handlers=[HandlerMergeModels()])
