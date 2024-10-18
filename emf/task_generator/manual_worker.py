d4bb73613c358bac9d9fe829e208c38f2626bb13
gintautas.poderys <gintautasp25@gmail.com>
2024-10-18 15:34:27 +0300

    #177 refactored manual task generation config parsing

--- emf/task_generator/manual_worker.py
+++ emf/task_generator/manual_worker.py
@@ -1,7 +1,6 @@
 import json
 import config
 import logging
-import pandas as pd
 from emf.task_generator.task_generator import generate_tasks, filter_and_flatten_dict
 from emf.common.integrations import rabbit
 from emf.common.config_parser import parse_app_properties
@@ -12,43 +11,42 @@ elk_handler = initialize_custom_logger()
 
 parse_app_properties(globals(), config.paths.task_generator.manual_task_generator)
 
-timeframe_conf = config.paths.task_generator.manual_timeframe_conf
-process_conf = config.paths.task_generator.manual_process_conf
+timeframe_conf = config.paths.task_generator.timeframe_conf
+process_conf = config.paths.task_generator.process_conf
 
 process_config_json = json.load(process_conf)
 timeframe_config_json = json.load(timeframe_conf)
 
 if MERGE_TYPE == 'BA':
     merge_type = "RMM"
+    process_config_json.pop(0)
 elif MERGE_TYPE == 'EU':
     merge_type = "CGM"
-   
-process_config_json[0]['runs'][0]['process_id'] = f'https://example.com/processes/{merge_type}_CREATION'
+    process_config_json.pop(1)
+
 if TIME_HORIZON == '1D':
-    process_config_json[0]['runs'][0]['@id'] = f'https://example.com/runs/DayAhead{merge_type}'
-    process_config_json[0]['runs'][0]['time_frame'] = 'D-1'
-    timeframe_config_json[0]['@id'] = "https://example.com/timeHorizons/D-1"
+    process_config_json[0]['runs'] = [d for d in process_config_json[0]['runs'] if 'DayAhead' in d['@id']]
+    timeframe_config_json = [d for d in timeframe_config_json if 'D-1' in d['@id']]
 elif TIME_HORIZON == '2D':
-    process_config_json[0]['runs'][0]['@id'] = f'https://example.com/runs/TwoDaysAhead{merge_type}'
-    process_config_json[0]['runs'][0]['time_frame'] = 'D-2'
-    timeframe_config_json[0]['@id'] = "https://example.com/timeHorizons/D-2"
+    process_config_json[0]['runs'] = [d for d in process_config_json[0]['runs'] if 'TwoDaysAhead' in d['@id']]
+    timeframe_config_json = [d for d in timeframe_config_json if 'D-2' in d['@id']]
 elif TIME_HORIZON == 'ID':
-    process_config_json[0]['runs'][0]['@id'] = f'https://example.com/runs/IntraDay{merge_type}/1'
-    process_config_json[0]['runs'][0]['time_frame'] = 'D-1'
-    timeframe_config_json[0]['@id'] = "https://example.com/timeHorizons/D-1"
+    process_config_json[0]['runs'] = [d for d in process_config_json[0]['runs'] if '1' in d['@id']]
+    timeframe_config_json = [d for d in timeframe_config_json if 'H-24' in d['@id']]
 elif TIME_HORIZON == 'WK':
-    process_config_json[0]['runs'][0]['@id'] = f'https://example.com/runs/WeekAhead{merge_type}'
-    process_config_json[0]['runs'][0]['time_frame'] = 'W-1'
-    timeframe_config_json[0]['@id'] = "https://example.com/timeHorizons/W-1"
+    process_config_json[0]['runs'] = [d for d in process_config_json[0]['runs'] if 'WeekAhead' in d['@id']]
+    timeframe_config_json = [d for d in timeframe_config_json if 'W-1' in d['@id']]
 
+process_config_json[0]['runs'][0]['run_at'] = '* * * * *'
 
-process_config_json[0]['runs'][0]['properties']['merge_type'] = MERGE_TYPE
+# process_config_json[0]['runs'][0]['properties']['merge_type'] = MERGE_TYPE
+# process_config_json[0]['runs'][0]['properties']['time_horizon'] = TIME_HORIZON
+# process_config_json[0]['runs'][0]['properties']['merging_entity'] = TASK_MERGING_ENTITY
+# process_config_json[0]['runs'][0]['properties']['mas'] = TASK_MAS
 process_config_json[0]['runs'][0]['properties']['included'] = [tso.strip() for tso in INCLUDED_TSO.split(',')] if INCLUDED_TSO else []
 process_config_json[0]['runs'][0]['properties']['excluded'] = [tso.strip() for tso in EXCLUDED_TSO.split(',')] if EXCLUDED_TSO else []
 process_config_json[0]['runs'][0]['properties']['local_import'] = [tso.strip() for tso in LOCAL_IMPORT.split(',')] if LOCAL_IMPORT else []
-process_config_json[0]['runs'][0]['properties']['time_horizon'] = TIME_HORIZON
 process_config_json[0]['runs'][0]['properties']['version'] = TASK_VERSION
-# process_config_json[0]['runs'][0]['properties']['mas'] = TASK_MAS
 process_config_json[0]['runs'][0]['properties']['replacement'] = RUN_REPLACEMENT
 process_config_json[0]['runs'][0]['properties']['scaling'] = RUN_SCALING
 process_config_json[0]['runs'][0]['properties']['upload_to_opdm'] = UPLOAD_TO_OPDM
@@ -57,10 +55,9 @@ process_config_json[0]['runs'][0]['properties']['send_merge_report'] = SEND_MERG
 process_config_json[0]['runs'][0]['properties']['pre_temp_fixes'] = PRE_TEMP_FIXES
 process_config_json[0]['runs'][0]['properties']['post_temp_fixes'] = POST_TEMP_FIXES
 
-process_config_json[0]['runs'][0]['properties']['merging_entity'] = TASK_MERGING_ENTITY
-timeframe_config_json[0]['period_start'] = f'{PROCESS_TIME_SHIFT}'
-timeframe_config_json[0]['period_duration'] = TASK_PERIOD_DURATION
-timeframe_config_json[0]['reference_time'] = TASK_REFERENCE_TIME
+# timeframe_config_json[0]['period_start'] = f'{PROCESS_TIME_SHIFT}'
+# timeframe_config_json[0]['period_duration'] = TASK_PERIOD_DURATION
+# timeframe_config_json[0]['reference_time'] = TASK_REFERENCE_TIME
 
 with open(process_conf, 'w') as file:
     json.dump(process_config_json, file, indent=1)
