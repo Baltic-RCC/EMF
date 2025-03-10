e1c32bbfb9b2760b69930c18af46772e2ed23c12
martynas.karobcikas <martynas.karobcikas@baltic-rcc.eu>
2025-03-10 16:17:25 +0200

    refactor #248: temporary commit during refactor

---
+++ emf/loadflow_tool/model_validator/worker.py
@@ -0,0 +1,29 @@
+import logging
+from uuid import uuid4
+from emf.common.logging import custom_logger
+from emf.loadflow_tool.model_validator.model_validator import HandlerModelsValidator
+from emf.common.integrations.elastic import HandlerSendToElastic
+
+# Initialize custom logger
+logger = logging.getLogger(__name__)
+elk_handler = custom_logger.initialize_custom_logger(extra={'worker': 'model-validator', 'worker_uuid': str(uuid4())})
+
+import config
+from emf.common.integrations import rabbit
+from emf.common.config_parser import parse_app_properties
+
+# Disabling triplets library logging at INFO level
+# logging.getLogger('triplets').setLevel(logging.WARNING)
+
+parse_app_properties(caller_globals=globals(), path=config.paths.model_validator.model_validator)
+
+# RabbitMQ consumer implementation
+consumer = rabbit.RMQConsumer(queue=INPUT_RMQ_QUEUE,
+                              message_handlers=[HandlerModelsValidator(),
+                                                # HandlerSendToElastic(),
+                                                ])
+
+try:
+    consumer.run()
+except KeyboardInterrupt:
+    consumer.stop()
