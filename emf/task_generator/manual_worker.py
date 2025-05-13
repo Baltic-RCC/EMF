77885d44ef06d860cfafb0d1f8ac395583eee015
martynas.karobcikas <mkarobcikas@gmail.com>
2025-05-13 14:10:19 +0300

    bugfix: removed unnecessary parameters of task generator

--- emf/task_generator/manual_worker.py
+++ emf/task_generator/manual_worker.py
@@ -75,7 +75,7 @@ tasks = list(generate_tasks(TASK_WINDOW_DURATION, TASK_WINDOW_REFERENCE, process
 # Publish tasks
 if tasks:
     logger.info(f"Creating connection to RMQ")
-    rabbit_service = rabbit.BlockingClient(host=RMQ_SERVER)
+    rabbit_service = rabbit.BlockingClient()
     logger.info(f"Sending tasks to Rabbit exchange: {RMQ_EXCHANGE}")
     for task in tasks:
         rabbit_service.publish(payload=json.dumps(task),
