b9bd84d94de0004e3a3e504f14048620a3c26124
MATISS <136691671+MK1295@users.noreply.github.com>
2025-10-29 15:04:53 +0200

    Update loadflow settings for active power limits
    load flow settings updated for EU default,EU relaxed and IGM

--- emf/common/loadflow_tool/loadflow_settings.py
+++ emf/common/loadflow_tool/loadflow_settings.py
@@ -21,9 +21,9 @@ OPENLOADFLOW_DEFAULT_PROVIDER = {
     'lowImpedanceBranchMode': 'REPLACE_BY_ZERO_IMPEDANCE_LINE', 
     'voltageRemoteControl': 'True',
     'loadPowerFactorConstant': 'True',  # cim:PowerFlowSettings.loadVoltageDependency TODO - check this
-    'plausibleActivePowerLimit': '5000.0',
+    'plausibleActivePowerLimit': '1900.0',
     'slackBusPMaxMismatch': '0.1', #slackBusDistributionThreshold
-    'voltagePerReactivePowerControl': 'False',
+    'voltagePerReactivePowerControl': 'True',
     'newtonRaphsonConvEpsPerEq': '1.0E-4',
     'voltageTargetPriorities': 'GENERATOR,TRANSFORMER,SHUNT',
     'transformerVoltageControlMode': 'AFTER_GENERATOR_VOLTAGE_CONTROL',  
@@ -35,7 +35,6 @@ OPENLOADFLOW_DEFAULT_PROVIDER = {
     'reactiveRangeCheckMode': 'MAX',
     'networkCacheEnabled': 'False',
     'svcVoltageMonitoring': 'False',
-    # 'stateVectorScalingMode': None,
     'maxSlackBusCount': '1',  # TODO - check this
     # 'debugDir': '',
     'incrementalTransformerVoltageControlOuterLoopMaxTapShift': '3',  # TODO - check this
@@ -61,7 +60,16 @@ OPENLOADFLOW_DEFAULT_PROVIDER = {
     'voltageInitModeOverride': 'FULL_VOLTAGE',
     # Fix Kirchoff 1st law error
     'slackDistributionFailureBehavior': 'FAIL',
+    #new
     'dcPowerFactor': '1.0',
+    'transformerReactivePowerControl': 'True',
+    'useLoadModel': 'False',
+    'useActiveLimits': 'True',
+    'lineSearchStateVectorScalingMaxIteration': '10',
+    'lineSearchStateVectorScalingStepFold': '1.33',
+    'maxVoltageChangeStateVectorScalingMaxDv': '0.1',
+    'maxVoltageChangeStateVectorScalingMaxDphi': '0.1745',
+
 }
 
 OPENLOADFLOW_DEFAULT = pypowsybl.loadflow.Parameters(
@@ -103,7 +111,7 @@ __IGM_VALIDATION_PROVIDER = {
     'disableVoltageControlOfGeneratorsOutsideActivePowerLimits': 'true', # supress q part of igm-ssh-vs-cgm-ssh error
 }
 __EU_DEFAULT_PROVIDER = {
-    'slackBusSelectionMode': 'MOST_MESHED',
+    'slackBusSelectionMode': 'LARGEST_GENERATOR',
     'referenceBusSelectionMode':'GENERATOR_REFERENCE_PRIORITY',
     'generatorReactivePowerRemoteControl': 'True',
     'reactivePowerRemoteControl': 'True',
@@ -116,16 +124,17 @@ __EU_DEFAULT_PROVIDER = {
     'maxReactivePowerMismatch': '0.1',  # cim:PowerFlowSettings.reactivePowerTolerance "0.1"
     'maxVoltageMismatch': '0.0001',  # cim:PowerFlowSettings.voltageTolerance "0.0001"
     'maxAngleMismatch': '1.0E-5',  # cim:PowerFlowSettings.voltageAngleLimit "10" TODO - How to convert
-    'slackBusPMaxMismatch': '0.09',  # To fulfill QOCDC SV_INJECTION_LIMIT = 0.1'
+    'slackBusPMaxMismatch': '0.1',  # To fulfill QOCDC SV_INJECTION_LIMIT = 0.1'
     'disableVoltageControlOfGeneratorsOutsideActivePowerLimits': 'true', # supress q part of igm-ssh-vs-cgm-ssh error
     'disableInconsistentVoltageControls': 'true',
     'transformerVoltageControlMode': 'INCREMENTAL_VOLTAGE_CONTROL',
     'shuntVoltageControlMode': 'INCREMENTAL_VOLTAGE_CONTROL',
     'phaseShifterControlMode': 'INCREMENTAL',
+    
 
 }
 __EU_RELAXED_PROVIDER = {
-    'slackBusSelectionMode': 'MOST_MESHED',
+    'slackBusSelectionMode': 'LARGEST_GENERATOR',
     'referenceBusSelectionMode':'GENERATOR_REFERENCE_PRIORITY',
     'generatorReactivePowerRemoteControl': 'True',
     'reactivePowerRemoteControl': 'True',
@@ -238,28 +247,30 @@ EU_DEFAULT = pypowsybl.loadflow.Parameters(
     read_slack_bus=True,
     write_slack_bus=False,
     distributed_slack=True,  #cim:PowerFlowSettings.slackDistributionKind cim:SlackDistributionKind.generationDistributionActivePowerAndVoltageNodesOnly
-    balance_type=pypowsybl._pypowsybl.BalanceType.PROPORTIONAL_TO_CONFORM_LOAD, #cim:PowerFlowSettings.slackDistributionKind cim:SlackDistributionKind.generationDistributionActivePowerAndVoltageNodesOnly
-    dc_use_transformer_ratio=None,
+    balance_type=pypowsybl._pypowsybl.BalanceType.PROPORTIONAL_TO_GENERATION_REMAINING_MARGIN, #cim:PowerFlowSettings.slackDistributionKind cim:SlackDistributionKind.generationDistributionActivePowerAndVoltageNodesOnly
+    dc_use_transformer_ratio=True,
     countries_to_balance=None,
     connected_component_mode=pypowsybl._pypowsybl.ConnectedComponentMode.ALL,
     provider_parameters=EU_DEFAULT_PROVIDER,
+    dc_power_factor=1.0,
 )
 
 EU_RELAXED = pypowsybl.loadflow.Parameters(
     voltage_init_mode=pypowsybl._pypowsybl.VoltageInitMode.UNIFORM_VALUES,  # cim:PowerFlowSettings.flatStart "true"
     transformer_voltage_control_on=True,  # cim:PowerFlowSettings.transformerRatioTapControlPriority "0"
     use_reactive_limits=False,  # cim:PowerFlowSettings.respectReactivePowerLimits "false"
-    phase_shifter_regulation_on=True,  # cim:PowerFlowSettings.transformerPhaseTapControlPriority "0"
+    phase_shifter_regulation_on=True,  # cim:PowerFlowSettings.transformerPhaseTapControlPriority "1"
     twt_split_shunt_admittance=None,
-    shunt_compensator_voltage_control_on=True,  # cim:PowerFlowSettings.switchedShuntControlPriority "0"
+    shunt_compensator_voltage_control_on=True,  # cim:PowerFlowSettings.switchedShuntControlPriority "2"
     read_slack_bus=True,
     write_slack_bus=False,
-    distributed_slack=True,  # cim:PowerFlowSettings.slackDistributionKind cim:SlackDistributionKind.generationDistributionActivePowerAndVoltageNodesOnly
-    balance_type=pypowsybl._pypowsybl.BalanceType.PROPORTIONAL_TO_CONFORM_LOAD,  #cim:PowerFlowSettings.slackDistributionKind cim:SlackDistributionKind.generationDistributionActivePowerAndVoltageNodesOnly
-    dc_use_transformer_ratio=None,
+    distributed_slack=True,  #cim:PowerFlowSettings.slackDistributionKind cim:SlackDistributionKind.generationDistributionActivePowerAndVoltageNodesOnly
+    balance_type=pypowsybl._pypowsybl.BalanceType.PROPORTIONAL_TO_GENERATION_REMAINING_MARGIN, #cim:PowerFlowSettings.slackDistributionKind cim:SlackDistributionKind.generationDistributionActivePowerAndVoltageNodesOnly
+    dc_use_transformer_ratio=True,
     countries_to_balance=None,
     connected_component_mode=pypowsybl._pypowsybl.ConnectedComponentMode.ALL,
-    provider_parameters=EU_RELAXED_PROVIDER,
+    provider_parameters=EU_DEFAULT_PROVIDER,
+    dc_power_factor=1.0,
 )
 
 ## Baltic merge parameters
